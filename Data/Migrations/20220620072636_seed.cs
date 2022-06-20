57146b28ac4102efd3654dcd89bb619e44a8102f
Alessandro Feoli <afeoli07@gmail.com>
2022-06-20 02:44:31 -0600

    Add project files.

---
+++ Data/Migrations/20220620072636_seed.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VerySmallCRM.Data.Migrations
+{
+    public partial class seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerName = table.Column<string>(nullable: true),
+                    CustomerEmail = table.Column<string>(nullable: true),
+                    CustomerPhone = table.Column<int>(nullable: false),
+                    CustomerNotes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customer", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customer");
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace VerySmallCRM.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public int CustomerPhone { get; set; }
+        public string CustomerNotes { get; set; }
+
+        public Customer()
+        {
+
+        }
+    }
+}
